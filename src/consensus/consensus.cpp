03022b76b9c078cf0d89274154cdf43f10bf4b58
zhaoyiyong <zhaoyy1010@gmail.com>
2018-05-17 18:06:38 +0800

    add testnet env

---
+++ src/consensus/consensus.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2017-2018 The BCO Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <consensus/consensus.h>
+
+// BCO fork information
+/** The starting height of the fork height. */
+static uint64_t BCO_FORK_BLOCK_HEIGHT = 0;
+static int64_t BCO_BLOCK_UNIXTIME_MIN = 0;
+
+void BCOUpdateConsensus(uint64_t nForkHeight, int64_t nBlockMinTimestamp)
+{
+    BCO_FORK_BLOCK_HEIGHT = nForkHeight;
+    BCO_BLOCK_UNIXTIME_MIN = nBlockMinTimestamp;
+}
+
+uint64_t BCOForkBlockHeight()
+{
+    return BCO_FORK_BLOCK_HEIGHT;
+}
+
+int64_t BCOBlockMinTimestamp()
+{
+    return BCO_BLOCK_UNIXTIME_MIN;
+}
