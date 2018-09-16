869ed8602efed121674309d900937df256505aec
zhaoyiyong <zhaoyy1010@gmail.com>
2018-09-16 18:51:20 +0800

    update max time adjust to 30min

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -1601,16 +1601,6 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
             return false;
         }
 
-        if (nTimeOffset > DEFAULT_MAX_TIME_ADJUSTMENT || nTimeOffset < -DEFAULT_MAX_TIME_ADJUSTMENT)
-        {
-            // disconnect from peers time deviation is greater than DEFAULT_MAX_TIME_ADJUSTMENT seconds
-            LogPrintf("peer=%d time deviation is greater than %d (%+d) seconds; disconnecting\n", pfrom->GetId(), DEFAULT_MAX_TIME_ADJUSTMENT, nTimeOffset);
-            connman->PushMessage(pfrom, CNetMsgMaker(INIT_PROTO_VERSION).Make(NetMsgType::REJECT, strCommand, REJECT_OBSOLETE,
-                                strprintf("Time deviation must be less than %d (%+d) seconds", DEFAULT_MAX_TIME_ADJUSTMENT, nTimeOffset)));
-            pfrom->fDisconnect = true;
-            return false;
-        }
-
         if (nVersion == 10300)
             nVersion = 300;
         if (!vRecv.empty())
