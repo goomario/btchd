9bb1922abada6a152dd648f4d3985dd7ed24799e
zhaoyiyong <zhaoyy1010@gmail.com>
2019-04-19 16:01:15 +0800

    remove special version

--- src/init.cpp
+++ src/init.cpp
@@ -1301,7 +1301,7 @@ bool AppInitMain()
             return InitError(strprintf(_("User Agent comment (%s) contains unsafe characters."), cmt));
         uacomments.push_back(cmt);
     }
-    strSubVersion = FormatSubVersion(CLIENT_NAME, CLIENT_VERSION_SHORT, uacomments);
+    strSubVersion = FormatSubVersion(CLIENT_NAME, CLIENT_VERSION, uacomments);
     if (strSubVersion.size() > MAX_SUBVERSION_LENGTH) {
         return InitError(strprintf(_("Total length of network version string (%i) exceeds maximum length (%i). Reduce the number or size of uacomments."),
             strSubVersion.size(), MAX_SUBVERSION_LENGTH));
