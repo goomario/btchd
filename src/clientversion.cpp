2365490849ec45be7a66c6806c987197c0a1a10f
felix <felixmariobrother@gmail.com>
2019-08-15 22:54:31 +0800

    update client version format

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -70,7 +70,7 @@ const std::string CLIENT_BUILD(BUILD_DESC CLIENT_VERSION_SUFFIX);
 
 static std::string FormatVersion(int nVersion)
 {
-    if (nVersion % 2 == 0)
+    if (nVersion % 100 == 0)
         return strprintf("%d.%d.%d", nVersion / 1000000, (nVersion / 10000) % 100, (nVersion / 100) % 100);
     else
         return strprintf("%d.%d.%d.%d", nVersion / 1000000, (nVersion / 10000) % 100, (nVersion / 100) % 100, nVersion % 100);
