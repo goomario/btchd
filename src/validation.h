dc7f8dc30c7c085e63fe7a93991731c11b11cbc4
felix <felixmariobrother@gmail.com>
2019-09-19 16:07:34 +0800

    fixed prune mode params

--- src/validation.h
+++ src/validation.h
@@ -206,22 +206,22 @@ extern bool fPruneMode;
 /** Number of MiB of block files that we're trying to stay below. */
 extern uint64_t nPruneTarget;
 /** Block files containing a block-height within MIN_BLOCKS_TO_KEEP of chainActive.Tip() will not be pruned. */
-static const unsigned int MIN_BLOCKS_TO_KEEP = 288;
+static const unsigned int MIN_BLOCKS_TO_KEEP = 960;
 /** Minimum blocks required to signal NODE_NETWORK_LIMITED */
-static const unsigned int NODE_NETWORK_LIMITED_MIN_BLOCKS = 288;
+static const unsigned int NODE_NETWORK_LIMITED_MIN_BLOCKS = 960;
 
 static const signed int DEFAULT_CHECKBLOCKS = 36;
 static const unsigned int DEFAULT_CHECKLEVEL = 3;
 
-// Require that user allocate at least 550MB for block & undo files (blk???.dat and rev???.dat)
-// At 1MB per block, 288 blocks = 288MB.
-// Add 15% for Undo data = 331MB
-// Add 20% for Orphan block rate = 397MB
-// We want the low water mark after pruning to be at least 397 MB and since we prune in
+// Require that user allocate at least 2800MB for block & undo files (blk???.dat and rev???.dat)
+// At 2MB per block, 960 blocks = 1920MB.
+// Add 15% for Undo data = 2208MB
+// Add 20% for Orphan block rate = 2649MB
+// We want the low water mark after pruning to be at least 2649 MB and since we prune in
 // full block file chunks, we need the high water mark which triggers the prune to be
-// one 128MB block file + added 15% undo data = 147MB greater for a total of 545MB
-// Setting the target to > than 550MB will make it likely we can respect the target.
-static const uint64_t MIN_DISK_SPACE_FOR_BLOCK_FILES = 550 * 1024 * 1024;
+// one 128MB block file + added 15% undo data = 147MB greater for a total of 2796MB
+// Setting the target to > than 2800MB will make it likely we can respect the target.
+static const uint64_t MIN_DISK_SPACE_FOR_BLOCK_FILES = 2800 * 1024 * 1024;
 
 /** 
  * Process an incoming block. This only returns after the best known valid
