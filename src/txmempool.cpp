a3d1cde39d2112b9d7ab2b1ccad963dabd49ea57
felix <felixmariobrother@gmail.com>
2019-10-17 15:47:11 +0800

    add check level for tx verify

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -612,7 +612,8 @@ static void CheckInputsAndUpdateCoins(const CTransaction& tx, CCoinsViewCache& m
 {
     CValidationState state;
     CAmount txfee = 0;
-    bool fCheckResult = tx.IsCoinBase() || Consensus::CheckTxInputs(tx, state, mempoolDuplicate, prevTipView, spendheight, txfee, CAccountID(), false, Params().GetConsensus());
+    bool fCheckResult = tx.IsCoinBase() || Consensus::CheckTxInputs(tx, state, mempoolDuplicate, prevTipView, spendheight, txfee, CAccountID(),
+        Consensus::CheckTxLevel::CheckMempool, Params().GetConsensus());
     assert(fCheckResult);
     UpdateCoins(tx, mempoolDuplicate, 1000000);
 }
